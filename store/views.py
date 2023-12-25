fb8a06364d6404bbb282f7920ab731634586ea20
Ashok <gaireashok36@gmail.com>
2023-12-26 00:13:03 +0545

    added to cart done-added-comment

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.db.models import Sum
 from django.contrib.auth.models import User
@@ -15,9 +15,9 @@ def cart(request):
     user = request.user.customer
     order, _ = Order.objects.get_or_create(customer=user, complete=False)
     items = order.orderitems_set.all()
-    print("user",user)
-    print("order",order)
-    print("items",items)
+    print("user", user)
+    print("order", order)
+    print("items", items)
 
     # If you need to add new products to the order, you would do it here.
     # For example, adding a product to the order:
@@ -26,9 +26,14 @@ def cart(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
+
+
 def add_cart(request, product_id):
     user = request.user.customer
     product = get_object_or_404(Product, id=product_id)
+    '''
+    this doesnot raise error when product is not there and this raise error(product = Product.objects.get(id=product_id)
+    '''
     order, _ = Order.objects.get_or_create(customer=user, complete=False)
 
     # Find an existing cart item or create a new one
@@ -40,17 +45,22 @@ def add_cart(request, product_id):
     if not created:
         cart_item.quantity += 1  # Increment quantity if item exists
         cart_item.save()
-        print("cart_item:",cart_item)
+        print("cart_item:", cart_item)
         print("done")
-
 
     # Redirect to some page, e.g., the cart overview page
     return redirect('/')
+
+
 def checkout(request):
     user = request.user.customer
-    order , created =Order.objects.get_or_create(customer=user , complete = False)
-    item= order.orderitems_set.all()
+    order, created = Order.objects.get_or_create(customer=user, complete=False)
+    '''
+    iif there is not order or multiple order it raise error(order = Order.objects.get(customer=user, complete=False):)
+    but the upper one get_or_create doesnot raise erroe instead of error it create order if doesnot exits
+    '''
+    item = order.orderitems_set.all()
 
-    context={'items':item, 'order':order}
+    context = {'items': item, 'order': order}
 
-    return render(request, 'store/checkout.html',context)
+    return render(request, 'store/checkout.html', context)
